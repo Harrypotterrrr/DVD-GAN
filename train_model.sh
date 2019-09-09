a1e453d40056d94af30b43e0c60dcfc61baab177
Harrypotterrrr <jiahaolin19971119@126.com>
2019-09-09 11:34:33 -0700

    update running command

--- train_model.sh
+++ train_model.sh
@@ -1,1 +1,1 @@
-python3.5 main.py --batch_size 2  --adv_loss hinge --version biggan_lsun --parallel True --gpus 0,1 --use_tensorboard True
+python3.5 main.py --adv_loss hinge --version biggan_lsun --parallel True --gpus 0,2,5,6 --use_tensorboard True --ds_chn 16 --dt_chn 16 --g_chn 16  --n_frames 8 --k_sample 4 --batch_size 16
