5d44dd0dc1ae4ad552b55b661924062812c9b90b
vominhduc <vmduc.khtn@gmail.com>
2019-09-09 11:49:12 -0700

    update lr

--- parameter.py
+++ parameter.py
@@ -29,8 +29,8 @@ def get_parameters():
     parser.add_argument('--batch_size', type=int, default=4)
     parser.add_argument('--num_workers', type=int, default=12)
     parser.add_argument('--g_lr', type=float, default=0.0001)
-    parser.add_argument('--d_lr', type=float, default=0.0004)
-    parser.add_argument('--lr_decay', type=float, default=0.95)
+    parser.add_argument('--d_lr', type=float, default=0.0002)
+    parser.add_argument('--lr_decay', type=float, default=0.9999)
     parser.add_argument('--beta1', type=float, default=0.0)
     parser.add_argument('--beta2', type=float, default=0.9)
 
@@ -55,7 +55,7 @@ def get_parameters():
 
     # Step size
     parser.add_argument('--log_step', type=int, default=10)
-    parser.add_argument('--sample_step', type=int, default=100)
+    parser.add_argument('--sample_step', type=int, default=20)
     parser.add_argument('--model_save_step', type=float, default=1.0)
 
     # Dataloader
