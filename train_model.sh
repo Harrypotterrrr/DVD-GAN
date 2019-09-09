dc982deaede4e857f36c02e85c2a4ec694bd1503
Harrypotterrrr <jiahaolin19971119@126.com>
2019-09-09 16:49:50 -0700

    remove ^M and update running command

--- train_model.sh
+++ train_model.sh
@@ -1,1 +1,2 @@
-python3.5 main.py --adv_loss hinge --version biggan_lsun --parallel True --gpus 0,2,5,6 --use_tensorboard True --ds_chn 16 --dt_chn 16 --g_chn 16  --n_frames 8 --k_sample 4 --batch_size 16
+python3.5 main.py --adv_loss hinge --version biggan_lsun --parallel True --gpus 0,2,3,4 --use_tensorboard True --ds_chn 16 --dt_chn 16 --g_chn 16  --n_frames 8 --k_sample 4 --batch_size 16
+
