d8c4b6a4adb6217eb959eed0ea041a67589869cf
Harrypotterrrr <jiahaolin19971119@126.com>
2019-11-12 10:30:38 -0800

    prepare to release codes

--- Readme.md
+++ Readme.md
@@ -31,18 +31,18 @@ scripts/train_model.sh <runing_mode> <dataset_path>
 ## Dataset
 
 Process UCF-101
-- Step 1 (Done): Download dataset
-- Step 2 (Done): Convert from avi to jpg files using:
+- **Step 1**: Download dataset
+- **Step 2**: Convert from avi to jpg files using:
 python utils/video_jpg_ucf101_hmdb51.py avi_video_directory jpg_video_directory
-- Step 3 (Done): Generate n_frames files using:
+- **Step 3**: Generate n_frames files using:
 python utils/n_frames_ucf101_hmdb51.py jpg_video_directory
-- Step 4 (Done): Generate annotation file in json format similar to ActivityNet using:
-python utils/ucf101_json.py annotation_dir_path 
-**Note: To change the number of class:
+- **Step 4**: Generate annotation file in json format similar to ActivityNet using:
+python utils/ucf101_json.py annotation_dir_path
+
+**Note**: To change the number of class:
 - Modify classInd.txt to contain the expected class(es). For example:
 1 ApplyEyeMakeup
 2 ApplyLipstick
 3 Archery
 - Run step 4 only
 - The code in dataloader automatically skips the unsed videos.
-
--- scripts/train_model.sh
+++ scripts/train_model.sh
@@ -4,10 +4,14 @@ DATASET_PATH=$2
 
 if [[ $1 == "local" ]]; then
   var="python3.6 main.py --adv_loss hinge --parallel True --gpus 0 1 --num_workers 8 \
-  --use_tensorboard True --ds_chn 64 --dt_chn 64 --g_chn 64 --n_frames 8 --k_sample 4 --batch_size 8 \
+  --use_tensorboard True --ds_chn 32 --dt_chn 32 --g_chn 32 --n_frames 8 --k_sample 4 --batch_size 32 \
+  --g_lr 5e-4 \
+  --d_lr 5e-5 \
   --n_class 1 \
+  --lr_schr const \
+  --n_class 2
   --root_path /home/haolin/Dataset/UCF101 \
-  --annotation_path annotation/ucf101_1class_01.json \
+  --annotation_path annotation/ucf101_01.json \
   --log_path /home/haolin/Documents/outputs/logs \
   --model_save_path /home/haolin/Documents/outputs/models \
   --sample_path /home/haolin/Documents/outputs/samples \
@@ -30,14 +34,14 @@ elif [[ $1 == "vllab4" ]]; then
 
 elif [[ ($1 == "vllab2") || ($1 == "vllab3") ]]; then
   var="/home/potter/package/Python-3.5.2/python main.py --adv_loss hinge --parallel True --gpus 2 3 --num_workers 16 \
-  --use_tensorboard True --ds_chn 64 --dt_chn 64 --g_chn 64 --n_frames 8 --k_sample 4 --batch_size 8 \
-  --n_class 1 \
-  --root_path /tmp4/potter/UCF101 \
-  --annotation_path annotation/ucf101_1class_01.json \
-  --log_path /tmp4/potter/outputs/logs \
-  --model_save_path /tmp4/potter/outputs/models \
-  --sample_path /tmp4/potter/outputs/samples \
-  --g_lr 1e-4 --d_lr 1e-4 \
+  --use_tensorboard True --ds_chn 64 --dt_chn 64 --g_chn 64 --n_frames 8 --k_sample 4 --batch_size 6 \
+  --n_class 2 \
+  --root_path /tmp3/potter/UCF101 \
+  --annotation_path annotation/ucf101_01.json \
+  --log_path /tmp3/potter/outputs/logs \
+  --model_save_path /tmp3/potter/outputs/models \
+  --sample_path /tmp3/potter/outputs/samples \
+  --g_lr 4e-5 --d_lr 4e-5 \
   --lr_schr multi
   "
   echo $var
